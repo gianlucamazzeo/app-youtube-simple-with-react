675e9ae0a2e47fc189639cc63216f3db57e5eb7a
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-05 05:00:22 +0200

    add form on searchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div><SearchBar /></div>
+            <div className="ui container"><SearchBar /></div>
         );
     }
 }
