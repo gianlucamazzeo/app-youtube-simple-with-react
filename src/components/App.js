2371d79af1d682f63be61e4b940734a0e4d159db
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-05 12:37:29 +0200

    resolve conflict in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 //
 class App extends React.Component {
     state = { videos: [] };  // initialize state object array 
@@ -21,7 +22,7 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos.
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
