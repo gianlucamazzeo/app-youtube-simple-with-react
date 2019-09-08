6c2ddea1a4b81173c9f79338f36f9052f19e3341
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-08 10:02:23 +0200

    add css to the videoList

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({ video }) => {
     return (
-        <div className="item">
-            <img className="ui image" alt={video.snippet} src={video.snippet.thumbnails.medium.url} />
+        <div className="video-item item">
+            <img className="ui image" alt={video.snippet.title} src={video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
             </div>
