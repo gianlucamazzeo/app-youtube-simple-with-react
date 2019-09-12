d9200061fcea82ff184e4dedf89c3c0e3821eee5
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-12 12:37:34 +0200

    add semantic ui for columns formatting

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,7 +9,7 @@ const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
  return <div>
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe src={videoSrc} title={video.snippet.title} />
             </div>
             <div className="ui segment">
                 <h4 className="ui header"> {video.snippet.title} </h4>
